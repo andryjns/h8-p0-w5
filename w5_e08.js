32ba497869331e4510241d57149b6c7a553045c1
andryjns <andry_jns@yahoo.com>
2018-06-28 01:29:42 +0700

    [Exercises 8] Cari Pelaku

---
+++ w5_e08.js
@@ -0,0 +1,13 @@
+function cariPelaku(str) {
+    var re= /abc/ig
+    var result= str.match(re)
+    
+    return result.length
+}
+  
+  // TEST CASES
+  console.log(cariPelaku('mabcvabc')); // 2
+  console.log(cariPelaku('abcdabdc')); // 1
+  console.log(cariPelaku('bcabcac')); // 1
+  console.log(cariPelaku('abcabcabc')); // 3
+  console.log(cariPelaku('babcbacabc')); // 2
